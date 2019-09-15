e53e3839478c7d63688a274f7257e23f492524b0
wxy1228 <403812699@qq.com>
2019-09-15 19:20:03 +0800

    add sm4

---
+++ src/test/sm4test.c
@@ -0,0 +1,53 @@
+#include <openssl/sm4.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main()
+{
+    unsigned char key[16];
+    set_key(key);//随机生成key
+	unsigned char input[100000];
+	unsigned char output[100000];
+	sm4_context ctx;
+    /*
+	int N,choice;
+	unsigned long i;
+    printf("请选择明文的输入情况：1.不指定明文长度；2.指定明文长度\n");
+    scanf("%d",&choice);
+	switch(choice){
+	  case 1:
+          N=choice1(input);
+		  break;
+      case 2:
+		  N=choice2(input);
+		  break;
+	  default:
+		  printf("无效输入\n");
+		  N=0;
+		  break;
+	}*/
+    printf("请输入需要加密的字符串：");
+    int N,i;
+    N = choice1(input);
+    printf("明文长度：%d\n",N);
+
+	//encrypt standard testing vector
+	sm4_setkey_enc(&ctx,key);
+	sm4_crypt_ecb(&ctx,1,N,input,output);
+	printf("加密得到密文：");
+	for(i=0;i<N;i++)
+		printf("%02x ", output[i]);
+	printf("\n");
+ 
+	//decrypt testing
+	sm4_setkey_dec(&ctx,key);
+	sm4_crypt_ecb(&ctx,0,N,output,output);
+	printf("解密得到密文：");
+	for(i=0;i<N;i++)
+		printf("%c", output[i]);
+	printf("\n");
+ 
+    return 0;
+}
