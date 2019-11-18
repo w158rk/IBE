9b48d7ccc392528a44fe4ca59e964fcc0a22844e
Wrk <wrk15835@gmail.com>
2019-11-18 10:18:38 +0800

    add build.sh

---
+++ build.sh
@@ -0,0 +1,24 @@
+#/bin/bash
+
+mkdir -p .gmssl 
+cd .gmssl 
+if [ -d ".git" ]; then
+    git pull origin master 
+else 
+    git clone git@github.com:w158rk/GmSSL.git ./ 
+fi
+echo "clone the repository finished ... "
+
+mkdir -p build 
+cd build 
+../config --debug 
+make 
+export IBE_LIB=`pwd`
+
+echo "make the library finished ..."
+
+cd ../../
+mkdir -p build
+cd build 
+cmake -DCMAKE_BUILD_TYPE=Debug ../
+make
